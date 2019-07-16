da3b8af48bf478ec8bcce62f5a213e872f3a1e33
Joe Klause <joeklause@gmail.com>
2019-07-16 09:49:58 -0700

    working percentage

--- src/app.js
+++ src/app.js
@@ -54,25 +54,27 @@ quizButton.onclick = function() {
         } else if(isYes(hawaiiQuestion) === hawaiiAnswer) {
             correctAnswers += 1;
         } 
+
+        //get correctAnswers into percentage
+        correctAnswers = Math.round((correctAnswers / 3) * 100);
         
         // response creations
         let response = 'Well ' + name + ',';
         if(correctAnswers === 0) {
-            response += ' you might want to read that again considering you got ' + correctAnswers + ' answers correct.';
+            response += ' you might want to read that again considering you got ' + correctAnswers + '% answers correct.';
         } 
-        else if(correctAnswers === 1) {
-            response += ' considering you only got ' + correctAnswers + ' answer correct, you might want to rethink your life.';
+        else if(correctAnswers === 33) {
+            response += ' considering you only got ' + correctAnswers + '% answer correct, you might want to rethink your life.';
         } 
-        else if(correctAnswers === 2) {
-            response += ' you got ' + correctAnswers + ' answers right. That\'s an ok job, but it is not perfect.';
-
+        else if(correctAnswers === 67) {
+            response += ' you got ' + correctAnswers + '% answers right. That\'s an ok job, but it is not perfect.';
         } 
-        else if(correctAnswers === 3) {
-            response += ' you get me! You really get me! You got ' + correctAnswers + ' answers correct!!';
+        else if(correctAnswers === 100) {
+            response += ' you get me! You really get me! You got ' + correctAnswers + '% answers correct!!';
         }
         result.textContent = response;
         
-          // reset correct answers and name
+          // reset correct answers and name for next time throught quiz
         name = '';
         correctAnswers = 0;
     }  
