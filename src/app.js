e4609544e0de5b65a907421ad84228a8bc4a10f2
Joe Klause <joeklause@gmail.com>
2019-07-16 09:04:45 -0700

    working app

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ import isYes from '../src/is-yes.js';
 const quizButton = document.getElementById('quiz-button');
 const result = document.getElementById('quiz-result');
 
+//needed variables
 let name = '';
+let correctAnswers = 0;
+let response = 'Well' + name + ',';
 
 //event handlers
 quizButton.onclick = function() {
@@ -17,20 +20,42 @@ quizButton.onclick = function() {
 
      // double check user wants to take quiz
     const confirmed = confirm('Are you sure you are ready to take this quiz ' + name + '? I\'m a pretty complex dude afterall...');
-        if(!confirmed) {
-             return;
-        } else {
-            //ask user three questions
-         let correctAnswers = 0;
+    if(!confirmed) {
+        return;
+    } else {
+        //ask user three questions
 
         const pictureQuestion = prompt('Did I take the picture on this page with my phone (Y/N?)?');
         const pictureAnswer = false;
-         if(isYes(pictureQuestion) === pictureAnswer) {
-          correctAnswers += 1
-          
-         } 
-}; 
-    //     // My three yes or no questions:
-    //     // Did I take that picture with my phone!
-    //     // Am I excited about the umpcoming summer temperatures?
-    //     // Would I like to visit Hawaii as soon as possible?
+        if(isYes(pictureQuestion) === pictureAnswer) {
+            correctAnswers += 1;
+        } 
+        const temperatureQuestion = prompt('Am I excited about the umpcoming summer temperatures (Y/N?)?');
+        const temperatureAnswer = false;
+
+        if(isYes(temperatureQuestion) === temperatureAnswer) {
+            correctAnswers += 1;
+        } 
+        const hawaiiQuestion = prompt('Would I like to visit Hawaii as soon as possible (Y/N?)?');
+        const hawaiiAnswer = true;
+
+        if(isYes(hawaiiQuestion) === hawaiiAnswer) {
+            correctAnswers += 1;
+        } 
+        
+        if(correctAnswers === 0) {
+            response += ' you might want to read that again considering you got ' + correctAnswers + 'answers correct.';
+
+        } else if(correctAnswers === 1) {
+            response += ' considering you only got ' + correctAnswers + ' answer correct, you might want to rethink your life.';
+
+        } else if(correctAnswers === 2) {
+            response += ' you got ' + correctAnswers + ' answers rightt. That\'s an ok job, but it is not perfect.';
+
+        } else if(correctAnswers === 3) {
+            response += ' you get me! You really get me! You got ' + correctAnswers + ' answers correct!!';
+        }
+
+        result.textContent = response;
+    }  
+};
