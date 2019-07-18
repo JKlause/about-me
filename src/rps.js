0d8d83c5b154836fa10b88b6ed265b9390bf51c9
Joe Klause <joeklause@gmail.com>
2019-07-17 18:15:58 -0700

    working user and computer throw

--- src/rps.js
+++ src/rps.js
@@ -4,15 +4,17 @@ const winCount = document.getElementById('win-count');
 const loseCount = document.getElementById('lose-count');
 const throwButton = document.getElementById('throw-button');
 const message = document.getElementById('message');
+const compChoiceImg = document.getElementById('comp-choice-img');
 
 throwButton.addEventListener('click', () => {
     const computerThrow = getPlay();
     
+    const src = 'Assets/rps/' + computerThrow + '.jpg';
+    compChoiceImg.src = src;
+    compChoiceImg.classList.remove('hidden');
+
+    const userThrowButton = document.querySelector('input:checked');
 
 });
 
-// user chooses
-//user throws
-// computer throws
-// computer reveals throw
 // tally wins and losses of player
