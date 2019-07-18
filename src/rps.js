d4d70563f3a22853a1ae4aa1685ddaba8824c972
Phil Fitch <mostreluctantly@gmail.com>
2019-07-18 16:49:56 -0700

    refactored checkBet and betResult functions

--- src/rps.js
+++ src/rps.js
@@ -15,37 +15,10 @@ let winLose;
 
 
 throwButton.addEventListener('click', () => {
-    const computerThrow = getPlay();
-    const src = 'Assets/rps/' + computerThrow + '.jpg';
-
-    compChoiceImg.src = src;
-    revealOnPage(compChoiceImg);
-
-
     const userThrow = document.querySelector('input:checked');
-
-    if(userThrow.value === computerThrow) {
-        updateMessages(resultMessage, 'We Tied! Stop Copying Me...');
-        winLose = 'tie';
-    }
-
-    if(userThrow.value === rock && computerThrow === scissors ||
-        userThrow.value === paper && computerThrow === rock ||
-        userThrow.value === scissors && computerThrow === paper) {
-        updateMessages(resultMessage, 'I Lost! Stop Cheatin!');
-        wins++;
-        winCount.textContent = wins;
-        winLose = 'win';
-    }
-
-    if(computerThrow === rock && userThrow.value === scissors ||
-        computerThrow === paper && userThrow.value === rock ||
-        computerThrow === scissors && userThrow.value === paper) {
-        updateMessages(resultMessage, 'I Won! Loser...');
-        losses++;
-        loseCount.textContent = losses;
-        winLose = 'lose';
-    }
+    const computerThrow = getPlay();
+    showCompThrowImg(computerThrow);
+    gameResultUpdate(userThrow, computerThrow);
 });
 
 
@@ -78,59 +51,14 @@ loadBetButton.addEventListener('click', () => {
     updateMessages(betResults, 'Watch me win.');
     updateMessages(resultMessage, 'Make your choice, punk.');
 
-
-    if(userBetAmt > moneyUserHasAmt) {
-        alert('You don\'t have that much money!');
-        
-
-    } else if(userBetAmt === moneyUserHasAmt) {
-        const allInConfirmation = confirm('You\'re all in, are you sure about that, you can just walk away now??');
-
-        if(allInConfirmation === true) {
-            alert('Livin on the edge your bet has been placed. Make your choice and throw!');
-            turnOnButton(throwButton);
-            loadBetButton.disabled = true;
-        }
-
-
-    } else if(userBetAmt < moneyUserHasAmt) {
-        const betConfirmation = confirm('You bet ' + userBetAmt + '. Are you sure you want to make that bet?');
-
-        if(betConfirmation === true) {
-            alert('Your bet has been placed. Make your choice and throw!');
-            turnOnButton(throwButton);
-            turnOffButton(loadBetButton);
-        }
-    }
+    checkConfirmBet();
 });
-
 
 throwButton.addEventListener('click', () => {
     moneyUserHasAmt = parseInt(moneyUserHas.textContent);
     userBetAmt = parseInt(userBet.value);
 
-
-    if(startBettingGameButton.disabled === true && throwButton.disabled === false) {
-        if(winLose === 'win') {
-            moneyUserHas.textContent = (moneyUserHasAmt + userBetAmt);
-            updateMessages(betResults, 'How dare you take my money! Let\'s play again!');
-            turnOffButton(throwButton);
-            turnOnButton(loadBetButton);
-
-        } else if(winLose === 'lose') {
-            moneyUserHas.textContent = (moneyUserHasAmt - userBetAmt);
-            updateMessages(betResults, 'HAHAHA You lost your money to me! Let\'s play again!');
-            turnOffButton(throwButton);
-            turnOnButton(loadBetButton);
-
-        } else if(winLose === 'tie') {
-            moneyUserHas.textContent = moneyUserHasAmt;
-            updateMessages(betResults, 'We tied... but I\'ll get your money soon.');
-            turnOffButton(throwButton);
-            turnOnButton(loadBetButton);
-        }
-    }
-
+    betResult();
 
     if(moneyUserHas.textContent === '0') {
         turnOffButton(startBettingGameButton);
@@ -141,6 +69,87 @@ throwButton.addEventListener('click', () => {
     }
 });
 
+
+function betResult() {
+    if(startBettingGameButton.disabled === true && throwButton.disabled === false) {
+        if(winLose === 'win') {
+            moneyUserHas.textContent = (moneyUserHasAmt + userBetAmt);
+            updateMessages(betResults, 'How dare you take my money! Let\'s play again!');
+            turnOffButton(throwButton);
+            turnOnButton(loadBetButton);
+        }
+        else if(winLose === 'lose') {
+            moneyUserHas.textContent = (moneyUserHasAmt - userBetAmt);
+            updateMessages(betResults, 'HAHAHA You lost your money to me! Let\'s play again!');
+            turnOffButton(throwButton);
+            turnOnButton(loadBetButton);
+        }
+        else if(winLose === 'tie') {
+            moneyUserHas.textContent = moneyUserHasAmt;
+            updateMessages(betResults, 'We tied... but I\'ll get your money soon.');
+            turnOffButton(throwButton);
+            turnOnButton(loadBetButton);
+        }
+    }
+    if(moneyUserHas.textContent === '0') {
+        turnOffButton(startBettingGameButton);
+        turnOffButton(throwButton);
+        turnOffButton(loadBetButton);
+        updateMessages(betResults, 'You lost all your money. Go away.');
+        updateMessages(resultMessage, 'Time for you to leave!');
+    }
+}
+
+function checkConfirmBet() {
+    if(userBetAmt > moneyUserHasAmt) {
+        alert('You don\'t have that much money!');
+    }
+    else if(userBetAmt === moneyUserHasAmt) {
+        const allInConfirmation = confirm('You\'re all in, are you sure about that, you can just walk away now??');
+        if(allInConfirmation === true) {
+            alert('Livin on the edge your bet has been placed. Make your choice and throw!');
+            turnOnButton(throwButton);
+            loadBetButton.disabled = true;
+        }
+    }
+    else if(userBetAmt < moneyUserHasAmt) {
+        const betConfirmation = confirm('You bet ' + userBetAmt + '. Are you sure you want to make that bet?');
+        if(betConfirmation === true) {
+            alert('Your bet has been placed. Make your choice and throw!');
+            turnOnButton(throwButton);
+            turnOffButton(loadBetButton);
+        }
+    }
+}
+
+function showCompThrowImg(computerThrow) {
+    const src = 'Assets/rps/' + computerThrow + '.jpg';
+    compChoiceImg.src = src;
+    revealOnPage(compChoiceImg);
+}
+
+function gameResultUpdate(userThrow, computerThrow) {
+    if(userThrow.value === computerThrow) {
+        updateMessages(resultMessage, 'We Tied! Stop Copying Me...');
+        winLose = 'tie';
+    }
+    if(userThrow.value === rock && computerThrow === scissors ||
+        userThrow.value === paper && computerThrow === rock ||
+        userThrow.value === scissors && computerThrow === paper) {
+        updateMessages(resultMessage, 'I Lost! Stop Cheatin!');
+        wins++;
+        winCount.textContent = wins;
+        winLose = 'win';
+    }
+    if(computerThrow === rock && userThrow.value === scissors ||
+        computerThrow === paper && userThrow.value === rock ||
+        computerThrow === scissors && userThrow.value === paper) {
+        updateMessages(resultMessage, 'I Won! Loser...');
+        losses++;
+        loseCount.textContent = losses;
+        winLose = 'lose';
+    }
+}
 
 function updateMessages(el, message) {
     el.textContent = message;
